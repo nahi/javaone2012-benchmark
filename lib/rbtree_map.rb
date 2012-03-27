606f63d858e079a85e7559fd2439617a65005662
Hiroshi Nakamura <nahi@ruby-lang.org>
2012-03-27 16:02:10 +0900

    Move debugging definitions to separete file

--- lib/rbtree_map.rb
+++ lib/rbtree_map.rb
@@ -191,44 +191,4 @@ class RBTreeMap
   end
 end
 
-if $DEBUG
-class RBTreeMap
-  class RBTree
-    def dump_tree(io, indent = '')
-      @right.dump_tree(io, indent + '  ')
-      io << indent << sprintf("#<%s:0x%010x %s %s> => %s", self.class.name, __id__, @color, @key.inspect, @value.inspect) << $/
-      @left.dump_tree(io, indent + '  ')
-    end
-
-    def dump_sexp
-      left = @left.dump_sexp
-      right = @right.dump_sexp
-      if left or right
-        '(' + [@key, left || '-', right].compact.join(' ') + ')'
-      else
-        @key
-      end
-    end
-
-    class EmptyTree < RBTree
-      def dump_tree(io, indent = '')
-        # intentionally blank
-      end
-
-      def dump_sexp
-        # intentionally blank
-      end
-    end
-  end
-
-  def dump_tree(io = '')
-    @root.dump_tree(io)
-    io << $/
-    io
-  end
-
-  def dump_sexp
-    @root.dump_sexp || ''
-  end
-end
-end
+require 'rbtree_map_debug' if $DEBUG
