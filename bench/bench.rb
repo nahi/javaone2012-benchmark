869e1f325b34e642cc91fd6f4da805869511ad8b
Hiroshi Nakamura <nahi@ruby-lang.org>
2012-03-28 19:24:02 +0900

    Update benchmark script

--- bench/bench.rb
+++ bench/bench.rb
@@ -1,36 +1,22 @@
 # encoding: utf-8
 require_relative "../lib/rbtree_map"
 
-file = ARGV.shift or raise
+class BenchmarkApp
+  TIMES = 5
 
-def benchmark(&block)
-  start = Time.now
-  yield
-  (Time.now - start) *1000
-end
-
-print "動作確認..."
-map = RBTreeMap.newInstance
-File.open(file).each_line do |line|
-  key, value, height = line.chomp.split(',', 3)
-  map.put(key, value)
-end
-File.open(file).each_line do |line|
-  key, value, _ = line.split(',', 3)
-  if map.get(key) != value
-    raise "値の不一致 #{map.get(key)} != #{value} at line #{line}"
+  def run(file)
+    TIMES.times do
+      execute(file)
+    end
+    start = Time.now
+    execute(file)
+    puts (Time.now - start) * 1000
   end
-end
-puts "OK"
 
-times = 5
-puts "ウォームアップ... (#{times} 回)"
-(times + 1).times do |idx|
-  puts "ベンチマーク: " if idx == times
-  elapsed = benchmark {
+  def execute(file)
     map = RBTreeMap.newInstance
     File.open(file).each_line do |line|
-      key, value, height = line.chomp.split(',', 3)
+      key, value, _ = line.split(',', 3)
       map.put(key, value)
     end
     File.open(file).each_line do |line|
@@ -39,6 +25,8 @@ puts "ウォームアップ... (#{times} 回)"
         puts "wrong value! #{map.get(key)} != #{value}"
       end
     end
-  }
-  puts elapsed
+  end
 end
+
+file = ARGV.shift or raise
+BenchmarkApp.new.run(file)
